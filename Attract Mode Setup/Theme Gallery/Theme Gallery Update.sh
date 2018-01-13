814758c9e0a7b720caa8754bda453818c0ff49b9
HP2ThemeGallery <35388570+HP2ThemeGallery@users.noreply.github.com>
2018-01-12 19:28:40 -0800

    Create Theme Gallery Update.sh

---
+++ Attract Mode Setup/Theme Gallery/Theme Gallery Update.sh
@@ -0,0 +1,5 @@
+rm -rf /home/pi/.attract/Attract\ Mode\ Setup/Theme\ Gallery/flyers
+mkdir -p /home/pi/.attract/themefiles
+git clone https://github.com/haggistech/hp2_theme_gallery.git "/home/pi/.attract/themefiles/"
+cp -r /home/pi/.attract/themefiles/* /home/pi/.attract/
+rm -rf /home/pi/.attract/themefiles
